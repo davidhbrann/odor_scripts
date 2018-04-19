8807ced534a0439cce664df8511015d5b30a6697
David Brann <david_brann@g.harvard.edu>
2018-04-18 20:34:40 -0400

    update to use config files

--- DO_example.py
+++ DO_example.py
@@ -3,29 +3,31 @@ from pypulse import PulseInterface
 from daqface import DAQ
 
 
+def load_pkl(filename):
+    with open(filename, 'rb') as fn:
+        data = pickle.load(fn)
+    return data
+
+
+# file names
+trial_file = 'PulseBoy/TrialBanks/PWMTest.trialbank'
+config_file = 'PulseBoy/params.config'
+
+config = load_pkl(config_file)
 hardware_params = config['hardware_params']
 global_params = config['global_params']
 
-# load data
-file_conf = 'PulseBoy/TrialBanks/PWMTest.trialbank'
-with open(file_conf, 'rb') as fn:
-    arraydata = pickle.load(fn)
-
+arraydata = load_pkl(trial_file)
 trial_params = arraydata[0][1]
-
 
 pulses, t = PulseInterface.make_pulse(hardware_params['samp_rate'],
                                       global_params['global_onset'],
                                       global_params['global_offset'],
                                       trial_params)
 
-trial_daq = DAQ.DoAiMultiTask(hardware_params['analog_dev'], hardware_params['analog_channels'],
-                                       hardware_params['digital_dev'], hardware_params['samp_rate'],
-                                       len(t) / hardware_params['samp_rate'], pulses,
-                                       hardware_params['sync_clock'])
-
-trial_daq = daq.DigitalOut(hardware_params['digital_dev'], hardware_params['samp_rate'],
-                                       len(t) / hardware_params['samp_rate'], pulses,
-                                       hardware_params['sync_clock'])
+trial_daq = DAQ.DigitalOut(hardware_params['digital_dev'],
+                           hardware_params['samp_rate'],
+                           len(t) / hardware_params['samp_rate'], pulses,
+                           hardware_params['sync_clock'])
 
 trial_daq.DoTask()
